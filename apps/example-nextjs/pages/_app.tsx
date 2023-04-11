0ea22599e59daf4e63cd1d7d53c089134d9afba8
danvim <daniel.2345@hotmail.com>
2023-04-11 23:19:25 +0800

    Bundle generated CSS files

--- apps/example-nextjs/pages/_app.tsx
+++ apps/example-nextjs/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { themeClass } from '@vanilla-extract-rollup-example/ui';
 import { AppProps } from 'next/app';
+import '@vanilla-extract-rollup-example/ui/dist/assets/bundle.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <div className={themeClass}><Component {...pageProps} /></div>;
--- apps/example-vite/src/main.tsx
+++ apps/example-vite/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import '@vanilla-extract-rollup-example/ui/dist/assets/bundle.css'
 
 ReactDOM.render(
   <React.StrictMode>
